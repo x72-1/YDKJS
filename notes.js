c712314d1be9ebcd894fe1018be8e0125a7abdcc
Sean Arseneault <x72@protonmai.com>
2017-12-12 21:57:31 -0800

    More notes

--- notes.js
+++ notes.js
@@ -1,2 +1,94 @@
 //switch
 let moonPhase = 'full';
+switch (moonPhase) {
+  case 'full':
+    console.log('Howl!');
+    break;
+  case 'mostly full':
+    console.log('Arms and legs are getting hairier');
+    break;
+  case 'mostly new':
+    console.log('Back on two feet');
+    break;
+  default:
+    console.log('Invalid moon phase');
+    break;
+}
+
+//Ternary
+let isLocked = false;
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+//input
+var = prompt("question");
+
+//coercion
+var a = "42";
+var b = Number( a );
+
+console.log( a );	// "42"
+console.log( b );	// 42
+
+//variables
+const CONSTANT_VARIABLE = 42;
+var variableName = "string";
+let someVariable = 'string';
+
+//loops
+while (numOfCustomers > 0) {
+	console.log( "How may I help you?" );
+
+	// help the customer...
+
+	numOfCustomers = numOfCustomers - 1;
+}
+
+do {
+	console.log( "How may I help you?" );
+
+	// help the customer...
+
+	numOfCustomers = numOfCustomers - 1;
+} while (numOfCustomers > 0);
+
+for (var i = 0; i <= 9; i = i + 1) {
+	console.log( i );
+}
+
+//functions
+const TAX_RATE = 0.08;
+
+function calculateFinalPurchaseAmount(amt) {
+	// calculate the new amount with the tax
+	amt = amt + (amt * TAX_RATE);
+
+	// return the new amount
+	return amt;
+}
+
+var amount = 99.99;
+
+amount = calculateFinalPurchaseAmount( amount );
+
+console.log( amount.toFixed( 2 ) );		// "107.99"
+
+//objects
+var obj = {
+	a: "hello world",
+	b: 42,
+	c: true
+};
+
+obj.a;		// "hello world"
+obj.b;		// 42
+obj.c;		// true
+
+obj["a"];	// "hello world"
+obj["b"];	// 42
+obj["c"];	// true
